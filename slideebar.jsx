3c55cba2566ad3f421d72482a39d18e67019ef15
jallurisudheer <112714951+jallurisudheer@users.noreply.github.com>
2022-09-02 22:30:04 +0530

    Create slideebar.jsx

---
+++ slideebar.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useContext } from 'react';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import { SocketContext } from '../Context';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  gridContainer: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  container: {
+    width: '600px',
+    margin: '50px 0',
+    padding: 0,
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
+  },
+}));
+const Sidebar = ({ children }) => {
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState('');
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Paper elevation={10} style={{ padding: '10px 20px' }}>
+      <Paper elevation={10} style={{ padding: '10px 20px', border: '2px solid black' }}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container style={{ width: '100%' }} className={classes.gridContainer}>
+            <Grid item xs={12} md={6} style={{ padding: 20 }}>
+              <Typography gutterBottom variant="h6">Account Info</Typography>
+              <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+              <CopyToClipboard text={me} style={{ marginTop: 20 }}>
+                <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
+                  Copy Your ID
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            <Grid item xs={12} md={6} style={{ padding: 20 }}>
+              <Typography gutterBottom variant="h6">Make a call</Typography>
+              <TextField label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+              {callAccepted && !callEnded ? (
+                <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} style={{ marginTop: 20 }}>
+                  Hang Up
+                </Button>
+              ) : (
+                <Button variant="contained" color="primary" startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} style={{ marginTop: 20 }}>
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+        {children}
+      </Paper>
+    </Container>
+  );
+};
+export default Sidebar;
